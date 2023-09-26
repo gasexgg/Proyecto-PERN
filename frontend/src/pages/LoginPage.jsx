1cd1e3e5921c49b39c7392803d3ac6014de58cd1
Daniel Guerrero <50740422+DanielGuerrero03@users.noreply.github.com>
2023-09-25 23:19:41 -0300

    agrego todo lo creado en el frontend

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import { Card, Input, Button, Label } from "../components/ui";
+import { useForm } from "react-hook-form";
+import { useAuth } from "../context/AuthContext";
+
 
 function LoginPege() {
+  const { register, handleSubmit } = useForm();
+  const { signin } = useAuth();
+  const navigate = useNavigate();
+  const onSubmit = handleSubmit(async(data) => {
+    await signin(data);
+    navigate("/perfil");
+  });
+
   return (
-    <div>LoginPege</div>
-  )
+    <div className="h-[calc(100vh-64px)] flex items-center justify-center">
+      <Card>
+        <h1 className="text-4xl font-bold my-2 text-center">Inciar sesión</h1>
+
+        <form onSubmit={onSubmit}>
+          <Label htmlFor="email">Email</Label>
+          <Input
+            type="email"
+            placeholder="Ingrese su email"
+            {...register("email", {
+              required: true,
+            })}
+          ></Input>
+          <Label htmlFor="password">Contraseña</Label>
+          <Input
+            type="password"
+            placeholder="Ingrese su Contraseña"
+            {...register("password", {
+              required: true,
+            })}
+          ></Input>
+          <Button>Ingresar</Button>
+        </form>
+        <div className=" flex justify-between my-4">
+          <p>¿No tienes cuenta?</p>
+          <Link to="/register">Registrate</Link>
+        </div>
+      </Card>
+    </div>
+  );
 }
 
-export default LoginPege
+export default LoginPege;
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { Button, Card, Input, Label } from "../components/ui";
+import  { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
 
 function RegisterPage() {
+  
+  const { register, handleSubmit, formState: {errors} } = useForm();
+
+  const {signup} = useAuth();
+  const navigate = useNavigate();
+  const onSubmit = handleSubmit(async(data) => { 
+    await signup(data);
+    navigate("/perfil");
+  });
+
+
   return (
-    <div>RegisterPage</div>
-  )
+    <div className="h-[calc(100vh-64px)] flex items-center justify-center">
+      <Card>
+        <h3 className="text-4xl font-bold my-2">Registro</h3>
+        <form onSubmit={onSubmit}>
+          <Label htmlFor="name">Nombre</Label>
+          <Input placeholder="Ingrese su nombre"
+          {...register("name", {required:true})}></Input>
+
+        {
+          errors.name && <p className="text-red-500">Este campo es requerido</p>
+        }
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" placeholder="Ingrese su email"
+          {...register("email", {required:true})}></Input>
+
+        {
+          errors.email && <p className="text-red-500">Este campo es requerido</p>
+        }
+          <Label htmlFor="password">Contraseña</Label>
+          <Input type="password" placeholder="Ingrese su contraseña"
+          {...register("password", {required:true})}></Input>
+
+        {
+          errors.password && <p className="text-red-500">Este campo es requerido</p>
+        }
+          <Button>Registrarse</Button>
+        </form>
+        <div className=" flex justify-between my-4">
+          <p>¿Ya tienes cuenta?</p>
+          <Link to="/login">Inciar Sesión</Link>
+        </div>
+      </Card>
+    </div>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
